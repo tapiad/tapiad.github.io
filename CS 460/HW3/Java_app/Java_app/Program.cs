52510273ad8eb480b30d59258ee4bb9dc0fa252e
tapiad <dtapia12@mail.wou.edu>
2017-10-30 18:50:56 -0700

    added Java_app

---
+++ CS 460/HW3/Java_app/Java_app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Java_app
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
