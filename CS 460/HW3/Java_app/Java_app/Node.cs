d75cc81c72843fe4e10c3bc6c662db22760c58f1
tapiad <dtapia12@mail.wou.edu>
2017-11-01 13:43:45 -0700

    added xml comments

--- CS 460/HW3/Java_app/Java_app/Node.cs
+++ CS 460/HW3/Java_app/Java_app/Node.cs
@@ -2,21 +2,32 @@
 
 namespace Java_app
 {
-    /**
-     * A simple singly linked node class. This implementation comes from 
-     * before Java had Generics.
-     */
+    //****************************************
+    /// <summary>
+    /// A simple singly linked node class.
+    /// Represents the stack
+    /// </summary>
+    //****************************************
     class Node
     {
         public object data; //The Payload
         public Node next; //Reference to the next Node in the chain
 
+
+        /// <summary>
+        /// Initializes an empty stack <see cref="T:Java_app.Node"/> class.
+        /// </summary>
         public Node()
         {
             data = null;
             next = null;
         }
 
+        /// <summary>
+        /// Initializes a stack <see cref="T:Java_app.Node"/> class.
+        /// </summary>
+        /// <param name="data">objects of the stack.</param>
+        /// <param name="next">checks next object of the stack.</param>
         public Node(object data, Node next)
         {
             this.data = data;
