25b7495b6d277e579551a83af2c96e4eec55032a
tapiad <dtapia12@mail.wou.edu>
2017-11-01 14:46:24 -0700

    added xml comments

--- CS 460/HW3/Java_app/Java_app/Scanner.cs
+++ CS 460/HW3/Java_app/Java_app/Scanner.cs
@@ -2,15 +2,30 @@
 
 namespace Java_app
 {
+    /// <summary>
+    /// Operates the same way as a java Scanner.
+    /// </summary>
     class Scanner : System.IO.StringReader
     {
+        /// <summary>
+        /// Current word
+        /// </summary>
         string currentWord;
 
+
+        /// <summary>
+        /// Where it gets all the input.
+        /// </summary>
+        /// <param name="source">The source of input.</param>
         public Scanner(string source) : base(source)
         {
             ReadNextWord();
         }
 
+
+        /// <summary>
+        /// Reads the next element of the scanner.
+        /// </summary>
         private void ReadNextWord()
         {
             StringBuilder sb = new StringBuilder();
@@ -34,12 +49,25 @@ namespace Java_app
                 currentWord = null;
         }
 
+
+        /// <summary>
+        /// Checks if it has a next string.
+        /// </summary>
+        /// <returns>The next string.</returns>
         public string HasNextStr()
         {
             ReadNextWord();
             return currentWord;
         }
 
+
+        /// <summary>
+        /// Checks whether or not it has a next int.
+        /// </summary>
+        /// <returns>
+        /// <c>true</c>, if true, returns next in, 
+        /// <c>false</c> otherwise, return null.
+        /// </returns>
         public bool HasNextInt()
         {
             if (currentWord == null)
@@ -47,6 +75,11 @@ namespace Java_app
             return int.TryParse(currentWord, out int dummy);
         }
 
+
+        /// <summary>
+        /// Gives next int.
+        /// </summary>
+        /// <returns>Next int.</returns>
         public int NextInt()
         {
             try
@@ -59,6 +92,14 @@ namespace Java_app
             }
         }
 
+
+        /// <summary>
+        /// Checks if it has next double.
+        /// </summary>
+        /// <returns>
+        /// <c>true</c>, if true, return next double. 
+        /// <c>false</c> otherwise, null.
+        /// </returns>
         public bool HasNextDouble()
         {
             if (currentWord == null)
@@ -66,6 +107,11 @@ namespace Java_app
             return double.TryParse(currentWord, out double dummy);
         }
 
+
+        /// <summary>
+        /// Grabs next double
+        /// </summary>
+        /// <returns>Next double.</returns>
         public double NextDouble()
         {
             try
@@ -78,6 +124,14 @@ namespace Java_app
             }
         }
 
+
+        /// <summary>
+        /// checks if it has next element
+        /// </summary>
+        /// <returns>
+        /// <c>true</c>, if next was hased, 
+        /// <c>false</c> otherwise.
+        /// </returns>
         public bool HasNext()
         {
             return currentWord != null;
