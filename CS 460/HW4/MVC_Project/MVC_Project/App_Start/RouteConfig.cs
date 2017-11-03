2887788f2e4cbe0bae591dc8600e64710cca4023
tapiad <dtapia12@mail.wou.edu>
2017-11-02 20:49:41 -0700

    added HelloWorldController.cs file

--- CS 460/HW4/MVC_Project/MVC_Project/App_Start/RouteConfig.cs
+++ CS 460/HW4/MVC_Project/MVC_Project/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace MVC_Project
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{id}"
+            );
         }
     }
 }
