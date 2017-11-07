d84c6c45922d25527390000affc4e17dc31c19b6
tapiad <dtapia12@mail.wou.edu>
2017-11-06 17:50:13 -0800

    Added DMV Application to start HW5 Project

---
+++ CS 460/HW5/DMV_Application/DMV_Application.Tests/Controllers/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DMV_Application.Tests.Controllers
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
---
+++ CS 460/HW5/DMV_Application/DMV_Application/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="DMV_Application.Global" %>
