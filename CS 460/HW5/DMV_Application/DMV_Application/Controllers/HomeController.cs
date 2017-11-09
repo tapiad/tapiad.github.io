0233e3ee585b63d10d392ea3f938930a2016c692
tapiad <dtapia12@mail.wou.edu>
2017-11-08 18:24:43 -0800

    Tried to connect to DB

--- CS 460/HW5/DMV_Application/DMV_Application/Controllers/HomeController.cs
+++ CS 460/HW5/DMV_Application/DMV_Application/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.Web;
+using System.Web.Configuration;
 using DMV_Application.DAL;
 using DMV_Application.Models;
 
